28ce479d58a9c31834e5fc22c8e8d02da4bda3da
coderpravin <impravin00@gmail.com>
2026-05-02 10:29:52 +0530

    feat : Align login/signup buttons to right and fix NoReverseMatch in navbar

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+app_name ='users'
 urlpatterns = [
     path('register-user', views.register_user, name="register-user"),
     path('login-user', views.login_user, name="login-user"),
